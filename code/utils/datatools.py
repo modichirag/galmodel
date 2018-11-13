24dc7dd256345ccbc8abdf356d674a3f39aa7ddb
EiffL <fr.eiffel@gmail.com>
2018-11-12 18:59:49 -0800

    Adding resnet code

--- code/utils/datatools.py
+++ code/utils/datatools.py
@@ -302,7 +302,8 @@ def cubify(arr, newshape):
 def uncubify(arr, oldshape):
     N, newshape = arr.shape[0], arr.shape[1:]
     oldshape = np.array(oldshape)    
-    repeats = (oldshape / newshape).astype(int)
+    print(newshape, oldshape)
+    repeats = (oldshape // newshape).astype(int)
     tmpshape = np.concatenate([repeats, newshape])
     order = np.arange(len(tmpshape)).reshape(2, -1).ravel(order='F')
     return arr.reshape(tmpshape).transpose(order).reshape(oldshape)
